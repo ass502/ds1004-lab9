6247e3d19a113039f9be63635f4228d71dd006cc
Aditi Nair <asn264@nyu.edu>
2016-04-15 18:03:47 -0400

    loaded data

--- scatter.js
+++ scatter.js
@@ -1,7 +1,22 @@
-var dataSet = [];
+var dataset = [];
 d3.csv('car.csv', function(data) {
-  console.log(data);
-      dataSet =data;
+
+  data.forEach(function(d){
+
+    dataset.push({
+      "name": d.name,
+      "mpg": +d.mpg,
+      "cylinders": +d.cylinders,
+      "displacement": +d.displacement,
+      "horsepower": +d.horsepower,
+      "weight": +d.weight,
+      "acceleration": +d.acceleration,
+      "model.year": +d['model.year'],
+    });
+
+  });
+ // console.log(dataset)
+
         });
 
 
