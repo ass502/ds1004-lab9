31b97036ae1bb0c0de504f82dd413c65c2cc823b
Aditi Nair <asn264@nyu.edu>
2016-04-15 18:29:48 -0400

    plot

--- scatter.js
+++ scatter.js
@@ -1,3 +1,16 @@
+var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+var x = d3.scale.linear()
+    .range([0, width]);
+var y = d3.scale.linear()
+    .range([height, 0]);
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
 var dataset = [];
 d3.csv('car.csv', function(data) {
 
@@ -15,10 +28,29 @@ d3.csv('car.csv', function(data) {
     });
 
   });
- // console.log(dataset)
+  
+  // Compute the scales’ domains.
+  x.domain(d3.extent(dataset, function(d) { return d.mpg; })).nice();
+  y.domain(d3.extent(dataset, function(d) { return d.cylinders; })).nice();
+  // Add the x-axis.
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.svg.axis().scale(x).orient("bottom"));
+  // Add the y-axis.
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(d3.svg.axis().scale(y).orient("left"));
+  // Add the points!
+  svg.selectAll(".point")
+      .data(dataset)
+    .enter().append("path")
+      .attr("class", "point")
+      .attr("d", d3.svg.symbol().type("triangle-up"))
+      .attr("transform", function(d) { return "translate(" + x(d.mpg) + "," + y(d.cylinders) + ")"; });
 
-        });
 
 
+  console.log(dataset)
 
-
+  });
