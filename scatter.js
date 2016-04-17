c89d9165f48a07771b0cd05338472abb6f45b479
ass502 <ass502@nyu.edu>
2016-04-17 13:59:58 -0400

    comments cleaned up

--- scatter.js
+++ scatter.js
@@ -1,3 +1,4 @@
+//Function that updates chart when x-axis is changed
 var xchange = function() {
 
   sel1 = this.value
@@ -12,21 +13,17 @@ var xchange = function() {
   //Join new data with old elements
   var pts = svg.selectAll(".point").data(new_dataset);
 
-  pts.enter().append("class", "old");
-
   pts.attr("class", "point")
       .attr("d", d3.svg.symbol().type("circle"))
       .attr("transform", function(d) { return "translate(" + x(d[sel1]) + "," + y(d[sel2]) + ")"; });
-
-  pts.exit().transition().attr("old").remove();
-
 };
 
 
+//Function that updates chart when y-axis is changed
 var ychange = function() {
 
-
   sel2 = this.value
+
   //Compute y scale domain
   y.domain(d3.extent(new_dataset, function(d) { return d[sel2]; })).nice();
 
@@ -36,103 +33,52 @@ var ychange = function() {
   //Join new data with old elements
   var pts = svg.selectAll(".point").data(new_dataset);
 
-  pts.enter().append("class", "old");
-
   pts.attr("class", "point")
       .attr("d", d3.svg.symbol().type("circle"))
       .attr("transform", function(d) { return "translate(" + x(d[sel1]) + "," + y(d[sel2]) + ")"; });
-
-  pts.exit().transition().attr("old").remove();
-
 };
 
 
+//Function that updates chart when the "Query MPG" button is pressed
 var set_mpg = function(){
 
   mpg_min = d3.select("#mpg-min").node().value;
   mpg_max = d3.select("#mpg-max").node().value;
-
-  console.log(mpg_max)
-  console.log(mpg_min)
 
   new_dataset = dataset.filter(function (row) {
   return row.mpg <= mpg_max &&
          row.mpg >= mpg_min;
   });
 
-
-  //This needs cleaning up - probably the enter, exit, etc. framework?
-
-  //Draw the plot
   // Compute the scales’ domains.
   x.domain(d3.extent(new_dataset, function(d) { return d[sel1]; })).nice();
   y.domain(d3.extent(new_dataset, function(d) { return d[sel2]; })).nice();
 
-  // Add the x-axis.
-  /*svg.append("g")
-      .attr("class", "x_axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
-  // Add the y-axis.
-  svg.append("g")
-      .attr("class", "y_axis")
-      .call(yAxis);*/
-
-  //Change the x axis
+  //Change the x and y axes
   svg.select(".x_axis") 
     .call(xAxis);
 
   svg.select(".y_axis") // change the x axis
     .call(yAxis);
 
-  // Add the points
-  /*
-  svg.selectAll(".point")
-      .data(new_dataset)
-    .enter().append("path")
-      .attr("class", "point")
-      .attr("d", d3.svg.symbol().type("circle"))
-      .attr("transform", function(d) { return "translate(" + x(d[sel1]) + "," + y(d[sel2]) + ")"; })
-      //.attr("test", function(d) {return d["name"]})
-      .on("mouseover", function(d) {
-          d3.select('#hovered').text('Car name: ' + d["name"]);
-      })
-      .on("mouseout", function(d){
-          d3.select('#hovered').text('Roll over a point to see it\'s car name.');
-          //console.log(10)
-      });
-  */
 
   var pts = svg.selectAll(".point")
       .data(new_dataset);
 
-    pts.enter().append("path")
-      .attr("class", "point")
-      .attr("d", d3.svg.symbol().type("circle"))
-      .attr("transform", function(d) { return "translate(" + x(d[sel1]) + "," + y(d[sel2]) + ")"; })
-      //.attr("test", function(d) {return d["name"]})
-      .on("mouseover", function(d) {
-          d3.select('#hovered').text('Car name: ' + d["name"]);
-      })
-      .on("mouseout", function(d){
-          d3.select('#hovered').text('Roll over a point to see it\'s car name.');
-          //console.log(10)
-      });
+  pts.enter().append("path")
+    .attr("class", "point")
+    .attr("d", d3.svg.symbol().type("circle"))
+    .attr("transform", function(d) { return "translate(" + x(d[sel1]) + "," + y(d[sel2]) + ")"; })
+    //.attr("test", function(d) {return d["name"]})
+    .on("mouseover", function(d) {
+        d3.select('#hovered').text('Car name: ' + d["name"]);
+    })
+    .on("mouseout", function(d){
+        d3.select('#hovered').text('Roll over a point to see it\'s car name.');
+        //console.log(10)
+    });
 
   pts.exit().remove();
-  
-  /*
-  //Join new data with old elements
-  var pts = svg.selectAll(".point").data(new_dataset);
-
-  pts.enter().append("class", "old");
-
-  pts.attr("class", "point")
-      .attr("d", d3.svg.symbol().type("circle"))
-      .attr("transform", function(d) { return "translate(" + x(d[sel1]) + "," + y(d[sel2]) + ")"; });
-
-  pts.exit().transition().attr("old").remove();
-  */
 };
 
 
@@ -165,91 +111,82 @@ var yAxis = d3.svg.axis().scale(y)
 var dataset = [];
 d3.csv('car.csv', function(data) {
 
-  var columnNames = Object.keys(data[0]);
-  columnNames.splice(0,1);
-  columnNames.splice(columnNames.length-1);
+var columnNames = Object.keys(data[0]);
+columnNames.splice(0,1);
+columnNames.splice(columnNames.length-1);
 
-  var listX = d3.select("#sel-x").on('change',xchange).selectAll("option").data(columnNames).enter().append("option").text(function(d){return d;})
-      .attr("value",function(d){return d;});
+var listX = d3.select("#sel-x").on('change',xchange).selectAll("option").data(columnNames).enter().append("option").text(function(d){return d;})
+    .attr("value",function(d){return d;});
 
-  var listY = d3.select("#sel-y").on('change',ychange).selectAll("option").data(columnNames).enter().append("option").text(function(d){return d;})
-      .attr("value",function(d){return d;});
+var listY = d3.select("#sel-y").on('change',ychange).selectAll("option").data(columnNames).enter().append("option").text(function(d){return d;})
+    .attr("value",function(d){return d;});
 
-  //draw the plot once with default values
-  sel1 = d3.select("#sel-x").node().value;
-  sel2 = d3.select("#sel-y").node().value;
+//draw the plot once with default values
+sel1 = d3.select("#sel-x").node().value;
+sel2 = d3.select("#sel-y").node().value;
 
-  /*
-  //var mpg_min = d3.select("#mpg_min").attr("type","text").attr("value",0).attr("size",10);
-  document.getElementById('mpg-min').setAttribute('type','text');
-  document.getElementById('mpg-min').setAttribute('value','0');
-  document.getElementById('mpg-min').setAttribute('size','10');
 
-  document.getElementById('mpg-max').setAttribute('type','text');
-  document.getElementById('mpg-max').setAttribute('value','30');
-  document.getElementById('mpg-max').setAttribute('size','10');
-  */
+//Create a button variable that calls the set_mpg function everytime the button is clicked
+var button = d3.select("#update").on('click',set_mpg)
 
-  //Create a button variable that calls the set_mpg function everytime the button is clicked
-  var button = d3.select("#update").on('click',set_mpg)
+//Set these up temporarily? not sure if we can get around them being global
+mpg_min = d3.select("#mpg-min").node().value;
+mpg_max = d3.select("#mpg-max").node().value;
 
-  //Set these up temporarily? not sure if we can get around them being global
-  mpg_min = d3.select("#mpg-min").node().value;
-  mpg_max = d3.select("#mpg-max").node().value;
+data.forEach(function(d){
 
-  data.forEach(function(d){
+  dataset.push({
+    "name": d.name,
+    "mpg": +d.mpg,
+    "cylinders": +d.cylinders,
+    "displacement": +d.displacement,
+    "horsepower": +d.horsepower,
+    "weight": +d.weight,
+    "acceleration": +d.acceleration,
+    "model.year": +d['model.year'],
+  });
 
-    dataset.push({
-      "name": d.name,
-      "mpg": +d.mpg,
-      "cylinders": +d.cylinders,
-      "displacement": +d.displacement,
-      "horsepower": +d.horsepower,
-      "weight": +d.weight,
-      "acceleration": +d.acceleration,
-      "model.year": +d['model.year'],
+});
+
+new_dataset = dataset.filter(function (row) {
+return row.mpg <= mpg_max &&
+       row.mpg >= mpg_min;
+});
+
+//Draw the plot once with default values:
+
+// Compute the scales’ domains.
+x.domain(d3.extent(new_dataset, function(d) { return d[sel1]; })).nice();
+y.domain(d3.extent(new_dataset, function(d) { return d[sel2]; })).nice();
+
+// Add the x-axis.
+svg.append("g")
+    .attr("class", "x_axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
+// Add the y-axis.
+svg.append("g")
+    .attr("class", "y_axis")
+    .call(yAxis);
+
+// add the tooltip area to the webpage
+var tooltip = d3.select("body").append("div")
+  .attr("class", "tooltip")
+  .style("opacity", 0);
+
+// Add the points
+svg.selectAll(".point")
+    .data(new_dataset)
+  .enter().append("path")
+    .attr("class", "point")
+    .attr("d", d3.svg.symbol().type("circle"))
+    .attr("transform", function(d) { return "translate(" + x(d[sel1]) + "," + y(d[sel2]) + ")"; })
+    .on("mouseover", function(d) {
+        d3.select('#hovered').text('Car name: ' + d["name"]);
+    })
+    .on("mouseout", function(d){
+        d3.select('#hovered').text('Roll over a point to see it\'s car name.');
     });
-
-  });
-
-  new_dataset = dataset.filter(function (row) {
-  return row.mpg <= mpg_max &&
-         row.mpg >= mpg_min;
-  });
-  
-  //Draw the plot once with default values:
-  // Compute the scales’ domains.
-  x.domain(d3.extent(new_dataset, function(d) { return d[sel1]; })).nice();
-  y.domain(d3.extent(new_dataset, function(d) { return d[sel2]; })).nice();
-
-  // Add the x-axis.
-  svg.append("g")
-      .attr("class", "x_axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
-  // Add the y-axis.
-  svg.append("g")
-      .attr("class", "y_axis")
-      .call(yAxis);
-
-  // add the tooltip area to the webpage
-  var tooltip = d3.select("body").append("div")
-    .attr("class", "tooltip")
-    .style("opacity", 0);
-
-  // Add the points
-  svg.selectAll(".point")
-      .data(new_dataset)
-    .enter().append("path")
-      .attr("class", "point")
-      .attr("d", d3.svg.symbol().type("circle"))
-      .attr("transform", function(d) { return "translate(" + x(d[sel1]) + "," + y(d[sel2]) + ")"; })
-      .on("mouseover", function(d) {
-          d3.select('#hovered').text('Car name: ' + d["name"]);
-      })
-      .on("mouseout", function(d){
-          d3.select('#hovered').text('Roll over a point to see it\'s car name.');
-      });
-  });
+});
 
 
